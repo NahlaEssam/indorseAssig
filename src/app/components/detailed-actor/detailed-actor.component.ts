5c3239da7036ca8936939d9d38489c0bb943d613
NahlaEssam <nahlaessam@gmail.com>
2018-12-18 23:23:31 +0200

    get actor details and movie details

--- src/app/components/detailed-actor/detailed-actor.component.ts
+++ src/app/components/detailed-actor/detailed-actor.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ErrorService } from 'src/app/shared/services/error/error.service';
+import { ActorService } from 'src/app/shared/services/actor/actor.service';
+import { ActorDetails } from '../../shared/models/actor';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detailed-actor',
   templateUrl: './detailed-actor.component.html',
   styleUrls: ['./detailed-actor.component.scss']
 })
-export class DetailedActorComponent implements OnInit {
+export class DetailedActorComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  actorId: number;
+  actor: ActorDetails;
+  private sub: any;
+  constructor(public actorService: ActorService, public errorService: ErrorService, private route: ActivatedRoute) {
+    this.sub = this.route.params.subscribe(params => {
+      this.actorId = +params['id'];
+      if (this.actorId !== undefined) {
+        this.getActor();
+      }
+    });
+
+
+  }
 
   ngOnInit() {
+  }
+
+  getActor() {
+    this.actorService.getActor(this.actorId).subscribe(res => {
+      this.actor = res;
+    }, error => {
+      this.errorService.error.emit(error);
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
--- src/app/components/detailed-movie/detailed-movie.component.ts
+++ src/app/components/detailed-movie/detailed-movie.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MovieDetails } from '../../shared/models/movie';
+import { MovieService } from 'src/app/shared/services/movie/movie.service';
+import { ErrorService } from 'src/app/shared/services/error/error.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detailed-movie',
   templateUrl: './detailed-movie.component.html',
   styleUrls: ['./detailed-movie.component.scss']
 })
-export class DetailedMovieComponent implements OnInit {
+export class DetailedMovieComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  movieId: number;
+  movie: MovieDetails;
+  private sub: any;
+  constructor(public actorService: MovieService, public errorService: ErrorService, private route: ActivatedRoute) {
+    this.sub = this.route.params.subscribe(params => {
+      this.movieId = +params['id'];
+      if (this.movieId !== undefined) {
+        this.getMovie();
+      }
+    });
+
+
+  }
 
   ngOnInit() {
+  }
+
+  getMovie() {
+    this.actorService.getMovie(this.movieId).subscribe(res => {
+      this.movie = res;
+    }, error => {
+      this.errorService.error.emit(error);
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
